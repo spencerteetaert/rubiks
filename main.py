b640dcc8ca241f4b9b285a62578ae4b1fe64a581
jeffrey-boop <jeffrey.guo@mail.utoronto.ca>
2021-02-16 18:04:09 -0500

    added random stuff to main

--- main.py
+++ main.py
@@ -2,3 +2,5 @@
 Main app application. I'm imagining final product is a 3D cube similar to the one I demonstrated, 
 but uses our trained model/allows for choice of solver to compare. Still unsure on this one 
 '''
+
+# lol
