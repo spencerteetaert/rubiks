c387e55097772ecbcddea00afa17e63ad855eaaf
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2021-03-19 16:19:30 -0400

    Added rubiks neural net model. Added test for rubiks model

---
+++ __tests__/rubiks_model_test.py
@@ -0,0 +1,39 @@
+file_name = "rubiks_model_test"
+
+def run():
+    # Test code goes here 
+    from src.model.model import RubiksModel
+    import random
+    import numpy as np
+    import torch
+    ret = True
+    msg = ""
+
+    # Ensure model initialization 
+    try:
+        model = RubiksModel()
+    except:
+        msg += "ERR: Model unable to be initialized.\n"
+        return ret, msg
+
+    # Ensure forward pass
+    state = np.array([[random.randint(0, 5) for i in range(6)] * 3,\
+        [random.randint(0, 5) for i in range(6)] * 3,\
+            [random.randint(0, 5) for i in range(6)] * 3], dtype=int)
+    state = torch.unsqueeze(torch.tensor(state), 0)
+    try:
+        ret = model(state)
+        expected = torch.random([1, 21])
+        flag = 1/torch.all(ret.shape == expected.shape)
+    except:
+        msg += "ERR: Forward pass failed.\n"
+
+    return ret, msg
+
+if __name__=="__tests__."+file_name:
+    print("Testing {}.py...".format(file_name))
+    ret, msg = run()
+    if ret:
+        print("{}.py successful.\n".format(file_name))
+    else:
+        print("ERR: {}.py failed.\n{}".format(file_name, msg))
--- src/model/model.py
+++ src/model/model.py
@@ -1,9 +1,32 @@
 '''
 PyTorch model file
 '''
-import torch 
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
 import numpy
 
-class RubiksModel:
-    def __init__(self):
-        pass
+class RubiksModel(nn.Module):
+    def __init__(self, internal_dimensions=[800, 800, 320], dropout_rate=0.1):
+        super(RubiksModel, self).__init__()
+        self.name = 'RubiksModel'
+        self.net = nn.Sequential(
+            # 1,163,540 tunable parameters
+            nn.Linear(3*18*6, internal_dimensions[0]),
+            nn.ReLU(),
+            nn.Linear(internal_dimensions[0], internal_dimensions[1]),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_rate), # Use of dropout to encourage generlization. 
+                                # Used because of the large state space
+            nn.Linear(internal_dimensions[1], internal_dimensions[2]),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(internal_dimensions[2], 21)
+        )
+
+    def forward(self, x):
+        # Input: Nx3x18x6 one hot encoded rubiks cube state data
+        x = torch.flatten(x)
+
+        # Output: Nx21 one hot encoded heuristic prediction
+        return self.net(x)
